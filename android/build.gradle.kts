136a0eec4334c1a6827cfe6ff84a2694d8436f8d
chabiha <chabihamad6@gmail.com>
2026-07-30 13:49:47 +0100

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,28 +22,3 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
-// Force compileSdk = 36 sur CHAQUE module, y compris le plugin file_picker
-// (c'est LUI qui est coincé en 34, pas ton app). Technique "GroovyObject" :
-// on ne nomme AUCUNE classe Android (c'est ça qui cassait la lecture du
-// fichier avant), donc ce bloc ne peut PAS faire planter la config.
-subprojects {
-    afterEvaluate {
-        val androidExt = extensions.findByName("android")
-        if (androidExt is groovy.lang.GroovyObject) {
-            val attempts = listOf(
-                "compileSdkVersion" to 36,
-                "setCompileSdkVersion" to 36,
-                "setCompileSdk" to "android-36",
-                "setCompileSdk" to 36
-            )
-            for ((name, arg) in attempts) {
-                try {
-                    androidExt.invokeMethod(name, arg)
-                    break
-                } catch (_: Throwable) {
-                }
-            }
-        }
-    }
-}
